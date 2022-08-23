149edc247b7425b65c430c922bff4b6d3d9244ba
Yohan-Rodriguez <85138862+Yohan-Rodriguez@users.noreply.github.com>
2022-08-23 02:49:26 -0500

    Add files via upload
    Archivos .java Sprint 2

---
+++ Mintic_Ciclo3_Textilera.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.zulu.mintic_ciclo3_textilera;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mintic_Ciclo3_Textilera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
