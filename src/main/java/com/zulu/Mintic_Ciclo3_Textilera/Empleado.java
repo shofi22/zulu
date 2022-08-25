a27bef3aede48ad4c642e0621fe52a055612776c
Felipe Granda R <felipegranda@hotmail.com>
2022-08-24 22:21:12 -0500

    - Intancia Emmpleado creada. - Atributos creados. - Constructor creado. - Setter y Getters creados.

--- src/main/java/com/zulu/Mintic_Ciclo3_Textilera/Empleado.java
+++ src/main/java/com/zulu/Mintic_Ciclo3_Textilera/Empleado.java
@@ -1,4 +1,35 @@
 package com.zulu.Mintic_Ciclo3_Textilera;
 
 public class Empleado {
+    String nombres, apellidos, correo;
+
+    public Empleado(String nombres, String apellidos, String correo) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
