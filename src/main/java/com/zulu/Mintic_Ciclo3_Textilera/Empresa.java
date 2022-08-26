95f3ec46267a697c354770bfaa61b06cf079f59e
shofi22 <45369667+shofi22@users.noreply.github.com>
2022-08-26 21:48:54 +0100

    Update Empresa.java

--- src/main/java/com/zulu/Mintic_Ciclo3_Textilera/Empresa.java
+++ src/main/java/com/zulu/Mintic_Ciclo3_Textilera/Empresa.java
@@ -1,4 +1,44 @@
 package com.zulu.Mintic_Ciclo3_Textilera;
 
 public class Empresa {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private int NIT;
+
+    public Empresa(String nombre, String direccion, String telefono, int NIT){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.NIT = NIT;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getDireccion() {
+            return direccion;
+        }
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setTelefono (String telefono){
+        this.telefono = telefono;
+    }
+
+    public int getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(int NIT){
+        this.NIT = NIT;
+    }
 }
